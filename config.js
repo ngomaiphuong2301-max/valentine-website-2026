9bf154ed0154c36657cea159ef22003d4ebfa1b7
ngomaiphuong2301-max <ngomaiphuong2301@gmail.com>
2026-02-05 22:44:07 +0900

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me?",                                    // First interaction
+            text: "Will You Be My Valentine?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
             secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
